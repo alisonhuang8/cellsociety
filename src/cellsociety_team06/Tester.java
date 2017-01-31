38daa0e503ba4842b47e6ccd50fb2dff0c8f7ff6
Gideon Pfeffer <gap16@duke.edu>
2017-01-31 17:57:14 -0500

    test

--- src/cellsociety_team06/Tester.java
+++ src/cellsociety_team06/Tester.java
@@ -7,8 +7,10 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import subUnits.Alive;
 import subUnits.Blank;
 import subUnits.Burning;
+import subUnits.Burnt;
 
 public class Tester extends Application{
 
@@ -19,6 +21,7 @@ public class Tester extends Application{
 	private int width = 500;
 	private int height = 500;
 	private Group root = new Group();
+	private double catchChance = 0.4;
 	
 	public static void main(String[] args){
 		launch(args);
@@ -28,11 +31,18 @@ public class Tester extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		window = primaryStage;
 		window.setResizable(false);
-		window.setScene(getScene());
+		window.setScene(reset());
 		window.show();
 	}
 	
-	private Scene getScene(){
+	private Scene reset(){
+		root.getChildren().removeAll();
+		nextGrid = new Unit[10][10];
+		return getLifeScene();
+//		return getFireScene();
+	}
+	
+	private Scene getLifeScene(){
 		for(int i = 0; i < 10; i++){
 			for(int j = 0; j < 10; j++){
 				int r = rand.nextInt(4);
@@ -40,7 +50,24 @@ public class Tester extends Application{
 					curGrid[i][j] = new Blank((width * i)/10, (height * j)/10, width/10, height/10);
 				}
 				else{
+					curGrid[i][j] = new Alive((width * i)/10, (height * j)/10, width/10, height/10);
+				}
+				root.getChildren().add(curGrid[i][j]);
+			}
+		}
+		Scene scene = new Scene(root, width, height);
+		scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
+		return scene;
+	}
+	
+	private Scene getFireScene(){
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 10; j++){
+				if(i == 0 && j == 0){
 					curGrid[i][j] = new Burning((width * i)/10, (height * j)/10, width/10, height/10);
+				}
+				else{
+					curGrid[i][j] = new Alive((width * i)/10, (height * j)/10, width/10, height/10);
 				}
 				root.getChildren().add(curGrid[i][j]);
 			}
@@ -54,11 +81,16 @@ public class Tester extends Application{
 		if(code == KeyCode.SPACE){
 			updateGrid();
 		}
+		if(code == KeyCode.R){
+			window.setScene(reset());
+		}
+		
 	}
 	
 	private void updateGrid(){
 //		reverse();
 		life();
+//		fire();
 	}
 	
 	private void reverse(){
@@ -83,7 +115,7 @@ public class Tester extends Application{
 			for(int j = 0; j < 10; j++){
 				root.getChildren().remove(curGrid[i][j]);
 				int n = getAliveNeighbors(i, j);
-				if(curGrid[i][j].isBurning()){
+				if(curGrid[i][j].isAlive()){
 					if(n < 2 || n > 3){
 						nextGrid[i][j] = new Blank((width * i)/10, (height * j)/10, width/10, height/10);
 					}
@@ -93,7 +125,7 @@ public class Tester extends Application{
 				}
 				else{
 					if(n == 3){
-						nextGrid[i][j] = new Burning((width * i)/10, (height * j)/10, width/10, height/10);
+						nextGrid[i][j] = new Alive((width * i)/10, (height * j)/10, width/10, height/10);
 					}
 					else{
 						nextGrid[i][j] = new Blank((width * i)/10, (height * j)/10, width/10, height/10);
@@ -113,12 +145,50 @@ public class Tester extends Application{
 		
 		for(int x = 0; x < move1.length; x++){
 			int newI = i + move1[x];
-int newJ = j + move2[x];
-			if(newI >= 0 && newI < 10 && newJ >= 0 && newJ < 10 && curGrid[newI][newJ].isBurning()){
+			int newJ = j + move2[x];
+			if(newI >= 0 && newI < 10 && newJ >= 0 && newJ < 10 && curGrid[newI][newJ].isAlive()){
 				alive++;
 			}
 		}
 		return alive;
+	}
+	
+	private void fire(){
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 10; j++){
+				root.getChildren().remove(curGrid[i][j]);
+				int n = getBurningNeighbors(i, j);
+				if(curGrid[i][j].isAlive()){
+					if(((1.0 - (Math.pow(1.0 - catchChance, n))) * 100.0) > rand.nextInt(100)){
+						nextGrid[i][j] = new Burning((width * i)/10, (height * j)/10, width/10, height/10);
+					}
+					else{
+						nextGrid[i][j] = new Alive((width * i)/10, (height * j)/10, width/10, height/10);
+					}
+				}
+				if(curGrid[i][j].isBurning() || curGrid[i][j].isBurnt()){
+					nextGrid[i][j] = new Burnt((width * i)/10, (height * j)/10, width/10, height/10);
+				}
+				root.getChildren().add(nextGrid[i][j]);
+			}
+		}
+		curGrid = nextGrid;
+		nextGrid = new Unit[10][10];
+	}
+	
+	private int getBurningNeighbors(int i, int j){
+		int[] move1 = {-1, -1, -1, 0, 0, 1, 1, 1};
+		int[] move2 = {1, 0, -1, 1, -1, 1, 0, -1};
+		int burning = 0;
+		
+		for(int x = 0; x < move1.length; x++){
+			int newI = i + move1[x];
+			int newJ = j + move2[x];
+			if(newI >= 0 && newI < 10 && newJ >= 0 && newJ < 10 && curGrid[newI][newJ].isBurning()){
+				burning++;
+			}
+		}
+		return burning;
 	}
 	
 }
