8c7cd2ff4e2c7c42cd8d3a2c632d98f12608bd7f
ALISON <ah365@duke.edu>
2017-01-30 22:01:17 -0500

    created subclasses of Models and timlines

---
+++ src/cellsociety_team06/lifeModel.java
@@ -0,0 +1,27 @@
+package cellsociety_team06;
+
+import javafx.animation.Timeline;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class lifeModel extends Model {
+	
+	public lifeModel(Stage s, Timeline t){
+		super(s,t);
+	}
+
+	@Override
+	public Scene getNextScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setNextScene() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
